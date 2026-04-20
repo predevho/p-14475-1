15f40fa9e28bab622b5183a270c74e34910a32ac
predevho <dlagusgh7894@gmail.com>
2026-04-20 16:52:20 +0900

    Feat : t5~t7 뺄셈 테스트 통과

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,12 +2,17 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        String[] expressionBits = expression.split(" ");
 
         int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
+        int num2 = Integer.parseInt(expressionBits[2]);
 
-        return num1 + num2;
+        if (expressionBits[1].equals("+")) {
+            return num1 + num2;
+        } else if (expressionBits[1].equals("-")) {
+            return num1 - num2;
+        } else {
+            throw new RuntimeException("지원하지 않는 연산입니다.");
+        }
     }
-
 }
